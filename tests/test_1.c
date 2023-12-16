c99c9eb0785405364bd25e52e1b2987d28e4f91d
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-12-16 10:32:35 +0100

    >ww<

--- tests/test_1.c
+++ tests/test_1.c
@@ -10,6 +10,7 @@ int main() {
 
     //
     int year;
+    int wday;
 
     //
     printf("\n");
@@ -23,17 +24,17 @@ int main() {
 
     //
     printf("Date of first tuesday of April %d : ", year);
-    time_t datetime_for_winter_in_australia = wished_wday_in_choosen_month(year, 3, 2, 3, 0, 0, FIRST);
+    time_t datetime_for_winter_in_australia = wished_wday_in_choosen_month(year, 3, wday, 3, 0, 0, FIRST);
     printf("%s", asctime(gmtime(&datetime_for_winter_in_australia)));
 
     //
     printf("Date of first tuesday of October %d : ", year);
-    time_t datetime_for_summer_in_australia = wished_wday_in_choosen_month(year, 9, 2, 2, 0, 0, FIRST);
+    time_t datetime_for_summer_in_australia = wished_wday_in_choosen_month(year, 9, wday, 2, 0, 0, FIRST);
     printf("%s", asctime(gmtime(&datetime_for_summer_in_australia)));
 
     //
     printf("Date of last tuesday of September %d : ", year);
-    time_t datetime_for_summer_in_new_zealand = wished_wday_in_choosen_month(year, 8, 2, 2, 0, 0, LAST);
+    time_t datetime_for_summer_in_new_zealand = wished_wday_in_choosen_month(year, 8, wday, 2, 0, 0, LAST);
     printf("%s", asctime(gmtime(&datetime_for_summer_in_new_zealand)));
 
     //

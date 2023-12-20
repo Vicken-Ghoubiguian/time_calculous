82ae2cb03a4a1e780b2524a1d2033ad28ca74516
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2023-12-20 16:22:26 +0100

    For later 2...

--- tests/test_2.c
+++ tests/test_2.c
@@ -9,6 +9,55 @@
 int main() {
 
     //
+    int decades;
+    int years;
+    int months;
+    int weeks;
+    int days;
+    int hours;
+    int minutes;
+    int seconds;
+
+    //
+    printf("\n");
+
+    //
+    printf("Veuillez entrer le nombre de décennies que vous voulez : ");
+    scanf("%d", &decades);
+
+    //
+    printf("Veuillez entrer le nombre d'années que vous voulez : ");
+    scanf("%d", &years);
+
+    //
+    printf("Veuillez entrer le nombre de mois que vous voulez : ");
+    scanf("%d", &months);
+
+    //
+    printf("Veuillez entrer le nombre de semaines que vous voulez : ");
+    scanf("%d", &weeks);
+
+    //
+    printf("Veuillez entrer le nombre de jours que vous voulez : ");
+    scanf("%d", &days);
+
+    //
+    printf("Veuillez entrer le nombre de heures que vous voulez : ");
+    scanf("%d", &hours);
+
+    //
+    printf("Veuillez entrer le nombre de minutes que vous voulez : ");
+    scanf("%d", &minutes);
+
+    //
+    printf("Veuillez entrer le nombre de minutes que vous voulez : ");
+    scanf("%d", &minutes);
+
+    //
+    printf("Veuillez entrer le nombre de secondes que vous voulez : ");
+    scanf("%d", &seconds);
+
+    //
     time_t today = time(NULL);
     time_t calculations_results = calculations_on_date_and_time(gmtime(&today), 2 * decade);
 

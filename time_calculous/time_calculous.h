7aa2aff06e4c17a7a2583a8a26120f43de22f187
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-01-11 09:11:46 +0100

    Delete useless 'time_unit'

--- time_calculous/time_calculous.h
+++ time_calculous/time_calculous.h
@@ -19,12 +19,7 @@ enum numeral {
 enum time_unit {
     second = 1,
     minute = 60,
-    hour = 3600,
-    /*day = 86400,
-    week = 604800,
-    month = 2629800,
-    year = 31557600,
-    decade = 315576000*/
+    hour = 3600
 };
 
 //

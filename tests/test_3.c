781441579d63f3ac79d983540344640374ae7113
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-03-07 10:39:08 +0100

    YeahYeah...

--- tests/test_3.c
+++ tests/test_3.c
@@ -9,13 +9,20 @@
 int main() {
 
     //
+    int year;
+
+    //
+    printf("Veuillez entrer l'année que vous voulez : ");
+    scanf("%d", &year);
+
+    //
     time_t timestamp = time(NULL);
 
     //
-    int weeks_count = number_of_weeks_in_a_year_according_iso_norm(gmtime(&timestamp), 2021);
+    int weeks_count = number_of_weeks_in_a_year_according_iso_norm(gmtime(&timestamp), year);
 
     //
-    printf("Number of weeks in the year %d : %d.\n", 2021, weeks_count);
+    printf("Number of weeks in the year %d : %d.\n", year, weeks_count);
 
     //
     return 0;

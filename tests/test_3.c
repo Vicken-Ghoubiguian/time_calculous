b86987b773d82b8524f57b950b3cb99b13533754
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-03-07 12:29:27 +0100

    Fgg - comment in this...

--- tests/test_3.c
+++ tests/test_3.c
@@ -19,7 +19,7 @@ int main() {
     // To get the number of weeks in the wished year
     int weeks_count = number_of_weeks_in_a_year_according_iso_norm(gmtime(&timestamp), year);
 
-    //
+    // To display the number of weeks in the wished year
     printf("Number of weeks in the year %d : %d.\n", year, weeks_count);
 
     //

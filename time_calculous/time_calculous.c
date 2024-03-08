3e48ed8cb5a344756ed20217897b0132f94b1676
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-03-08 19:45:52 +0100

    AgggHtyY

--- time_calculous/time_calculous.c
+++ time_calculous/time_calculous.c
@@ -264,6 +264,10 @@ int number_of_weeks_in_a_year_according_to_the_iso_norm(struct tm *datetime, int
 time_t wished_wday_in_choosen_year(int year, int wday, int number_of_weekday_in_the_year)
 {
     //
+    time_t timestamp = time(NULL);
+    int weeks_count = number_of_weeks_in_a_year_according_to_the_iso_norm(gmtime(&timestamp), year);
+
+    //
     /*int i = 0;
     int condition = 0;
     int i_of_week = 1;

36bb4573b18b3e04f2e9b354743e520827cd0eb2
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-03-15 21:39:14 +0100

    AnbN

--- tests/test_3_bis.c
+++ tests/test_3_bis.c
@@ -14,20 +14,20 @@ int main() {
     time_t timestamp = time(NULL);
 
     //
-    printf("\n\n year  | number of weeks \n");
+    printf("\n\n| year  | number of weeks |\n");
 
     //
     while(year < 2100)
     {
         //
-        printf(" %d  |    %d    \n", year, number_of_weeks_in_a_year_according_to_the_iso_norm(gmtime(&timestamp), year));
+        printf("| %d  |    %d           |\n", year, number_of_weeks_in_a_year_according_to_the_iso_norm(gmtime(&timestamp), year));
 
         //
         year++;
     }
 
     //
-    printf("________________________\n\n");
+    printf("|_________________________|\n\n");
 
     //
     return 0;

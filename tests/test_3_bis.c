542c91bc6567433ba371dd34cb107e734c96c60d
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-03-15 22:08:55 +0100

    WxCC

--- tests/test_3_bis.c
+++ tests/test_3_bis.c
@@ -15,6 +15,7 @@ int main() {
     //
     int weeks_count;
     int year = 2000;
+    int number_of_weeks_in_the_year;
     time_t timestamp = time(NULL);
 
     //
@@ -26,7 +27,20 @@ int main() {
     while(year < 2100)
     {
         //
-        printf("| %d  |    %d           |\n", year, number_of_weeks_in_a_year_according_to_the_iso_norm(gmtime(&timestamp), year));
+        number_of_weeks_in_the_year = number_of_weeks_in_a_year_according_to_the_iso_norm(gmtime(&timestamp), year);
+
+        //
+        if(number_of_weeks_in_the_year == 53)
+        {
+            //
+            printf("| \033[1m\033[31m %d \033[0m | \033[1m\033[31m   %d    \033[0m       |\n", year, number_of_weeks_in_the_year);
+        }
+        //
+        else
+        {
+            //
+            printf("| \033[1m\033[32m %d \033[0m | \033[1m\033[32m   %d    \033[0m       |\n", year, number_of_weeks_in_the_year);
+        }
 
         //
         year++;

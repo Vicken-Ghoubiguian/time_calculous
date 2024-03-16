c94fea24623d254ab14aac632dd28c2ee403ac31
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-03-16 19:11:51 +0100

    Others bis' tests

---
+++ tests/test_1_bis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Importation of the 'time_calculous' library
+#include "../time_calculous/time_calculous.h"
+
+//
+int main() {
+
+    //
+    return 0;
+}

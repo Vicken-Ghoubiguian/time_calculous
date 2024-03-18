8790895f4424eef493c5c8e732485957ef23a85f
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-03-18 19:43:44 +0100

    HJJJH

--- tests/test_3_bis.c
+++ tests/test_3_bis.c
@@ -14,33 +14,33 @@
 //
 int main() {
 
-    //
+    // Definition of all useful variables and their implementation for some of them
     int weeks_count;
     int year = 2000;
     int number_of_weeks_in_the_year;
     time_t timestamp = time(NULL);
 
-    //
+    // To display top of the array
     printf("\n___________________________\n");
     printf("| %s year %s |%s number of weeks %s|\n", BOLDBLUE, RESET, BOLDBLUE, RESET);
     printf("|________|_________________|\n");
 
-    //
+    // 
     while(year < 2100)
     {
         //
         number_of_weeks_in_the_year = number_of_weeks_in_a_year_according_to_the_iso_norm(gmtime(&timestamp), year);
 
-        //
+        // in the case where there are 53 weeks in the year...
         if(number_of_weeks_in_the_year == 53)
         {
-            //
+            // ...display in red...
             printf("| \033[1m\033[31m %d \033[0m | \033[1m\033[31m   %d    \033[0m       |\n", year, number_of_weeks_in_the_year);
         }
-        //
+        // ...and in the other case...
         else
         {
-            //
+            // ...display in green
             printf("| \033[1m\033[32m %d \033[0m | \033[1m\033[32m   %d    \033[0m       |\n", year, number_of_weeks_in_the_year);
         }
 
@@ -48,7 +48,7 @@ int main() {
         year++;
     }
 
-    //
+    // To display foot of the array
     printf("|________|_________________|\n\n");
 
     //

a96599e92732b709aeef9eabcf8329a675b3a59e
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-04-14 14:03:32 +0200

    Yeah...TTS

--- tests/test_3.c
+++ tests/test_3.c
@@ -19,8 +19,14 @@ int main() {
     // To get the number of weeks in the wished year
     int weeks_count = number_of_weeks_in_a_year_according_to_the_iso_norm(gmtime(&timestamp), year);
 
+    //
+    printf("\n");
+
     // To display the number of weeks in the wished year
-    printf("Number of weeks in the year %d : %d.\n", year, weeks_count);
+    printf("Number of weeks in the year %d : %d\n", year, weeks_count);
+
+    //
+    printf("\n");
 
     //
     return 0;

0743db7863c967dd6641e7cf6211ce1e8e3a24e5
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-04-29 09:32:36 +0200

    New example

---
+++ tests/test_6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Importation of the 'time_calculous' library
+#include "../time_calculous/time_calculous.h"
+
+// Initialisation of 'main' function
+int main() {
+
+    //
+    int n = number_of_days_in_choosen_month_in_choosen_year(1, 2023);
+
+    //
+    printf("%d\n", n);
+
+    //
+    return 0;
+}

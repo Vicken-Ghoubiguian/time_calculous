e88ec4a29688bdc59ffc8e1a8fa966b87087a597
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-05-09 20:01:34 +0200

    KLKL

--- time_calculous/time_calculous.c
+++ time_calculous/time_calculous.c
@@ -33,7 +33,7 @@ time_t calculations_on_date_and_time_from_today(int decades, int years, int mont
 
     // Definition of the 'cdatetime_min_time' which corresponds to the 'January 1 1900 00:00:00' date and time
     /*cdatetime_min_time = gmtime(&today);
-    cdatetime_min_time->tm_year = 0;
+    cdatetime_min_time->tm_year = 1900;
     cdatetime_min_time->tm_mon = 0;
     cdatetime_min_time->tm_mday = 1;
     cdatetime_min_time->tm_hour = 0;

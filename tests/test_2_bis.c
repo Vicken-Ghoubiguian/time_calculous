a4ba07a434d1ab794f78f7e8da09d893b4172f8a
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-05-26 19:49:55 +0200

    Correction of a bug

--- tests/test_2_bis.c
+++ tests/test_2_bis.c
@@ -9,6 +9,7 @@
 #define RESET "\033[0m"
 #define BOLDGREEN "\033[1m\033[32m"
 #define BOLDRED "\033[1m\033[31m"
+#define BOLDMAGENTA "\033[1m\033[35m"
 
 // Initialisation of 'main' function
 int main() {

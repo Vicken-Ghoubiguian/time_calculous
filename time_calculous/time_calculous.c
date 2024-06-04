68a60614850e1a9515269d16dc3863916619fac6
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-06-04 20:10:37 +0200

    1

--- time_calculous/time_calculous.c
+++ time_calculous/time_calculous.c
@@ -17,8 +17,8 @@ struct time_diff diffs_on_date_and_time(struct tm *first_dt, struct tm *second_d
     //
     calculation_result = first_dt_timestamp - second_dt_timestamp;
 
-    //
-    
+    // To calculate and encapsulate number of centuries, years, months, days, hours, minutes... results in the 'time_diff' struct
+
 
     //
     return td;

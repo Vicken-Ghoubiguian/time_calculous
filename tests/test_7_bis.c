ad32186006560d2bf40a448a0e0cb9d531f25f19
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-06-09 18:28:49 +0200

    TT

---
+++ tests/test_7_bis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Importation of the 'time_calculous' library
+#include "../time_calculous/time_calculous.h"
+
+// Initialisation of 'main' function
+int main() {
+
+    //
+    return 0;
+}

b1ab8afc382117cd490b5cd041eea532c57e9afb
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-06-09 20:03:58 +0200

    Definition of all needed variables for test 7...

--- tests/test_7.c
+++ tests/test_7.c
@@ -8,6 +8,24 @@
 // Initialisation of 'main' function
 int main() {
 
+    // 
+    int year_0;
+    int month_0;
+    int mday_0;
+    int hour_0;
+    int minute_0;
+    int second_0;
+    struct tm dt_0;
+    time_t timestamp_0;
+    time_t timestamp_1;
+    struct tm dt_1;
+    int year_1;
+    int month_1;
+    int mday_1;
+    int hour_1;
+    int minute_1;
+    int second_1;
+
     //
     struct time_diff td = diffs_on_date_and_time(1720549084, 1435207316);
 
